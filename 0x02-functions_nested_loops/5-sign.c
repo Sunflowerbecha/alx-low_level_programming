acd98c256a03a69917331d62a5dcc11b12d6ea0d
sunflowerbecha <sunflowerbecha@gmail.com>
2023-07-03 08:37:04 -0700

    100-times_table

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - prints the sign of a value
+ *
+ * @n: the value in question
+ *
+ * Return: (0) or (1) depending' or '-' for negative
  */
 int print_sign(int n)
 {
@@ -16,11 +15,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
-}}
+		return ('-');
+	}
+}
